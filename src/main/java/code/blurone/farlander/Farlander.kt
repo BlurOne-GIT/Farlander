49e0663483ad92fb1903c8f390f1fd2853d8943e
BlurOne-GIT <61806094+BlurOne-GIT@users.noreply.github.com>
2023-03-12 14:15:29 -0300

    Initial commit 2

---
+++ src/main/java/code/blurone/farlander/Farlander.kt
@@ -0,0 +1,13 @@
+package code.blurone.farlander
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Farlander : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
