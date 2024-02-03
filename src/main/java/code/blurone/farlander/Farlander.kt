58bf7df98a92761f32a349f51ca16ebdde584783
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-02-03 20:21:27 -0300

    build: project

---
+++ src/main/java/code/blurone/farlander/Farlander.kt
@@ -0,0 +1,15 @@
+package code.blurone.farlander
+
+import org.bukkit.generator.ChunkGenerator
+import org.bukkit.plugin.java.JavaPlugin
+
+class Farlander : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
